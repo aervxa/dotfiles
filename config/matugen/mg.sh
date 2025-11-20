491925a8d6a90cc0794d7d91f4894081a6256f0e
zorvi <aervxa@gmail.com>
2025-11-20 07:20:18 +0300

    .config

---
+++ config/matugen/mg.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+image="$1"
+
+if [ -z "$image" ]; then
+    echo "no image provided"
+    exit 1
+fi
+
+matugen image "$image"
+cp "$image" "$HOME/.config/matugen/current_wallpaper"
